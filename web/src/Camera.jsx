bdac028417b858292b031e351d0c64730fbf1cb2
suensky <suensky@outlook.com>
2021-02-16 21:42:19 -0800

    Update

--- web/src/Camera.jsx
+++ web/src/Camera.jsx
@@ -1,37 +1,190 @@
 import React from "react";
-import { Button, message } from "antd"
+import { Button, Select, message } from "antd"
+
+const { Option } = Select;
 
 const constraints = window.constraints = {
     audio: true,
     video: true
 }
 
+const constraints240p = {
+    video: {
+        width: {
+            exact: 320,
+        },
+        height: {
+            exact: 240,
+        }
+    }
+};
+
+const constraints480p = {
+    video: {
+        width: {
+            exact: 640,
+        },
+        height: {
+            exact: 480,
+        }
+    }
+};
+
+const constraints720p = {
+    video: {
+        width: {
+            exact: 1280,
+        },
+        height: {
+            exact: 720,
+        }
+    }
+};
+
+const constraints1080p = {
+    video: {
+        width: {
+            exact: 1920,
+        },
+        height: {
+            exact: 1080,
+        }
+    }
+};
+
+const constraints2k = {
+    video: {
+        width: {
+            exact: 2560,
+        },
+        height: {
+            exact: 1440,
+        }
+    }
+};
+
+const constraints4k = {
+    video: {
+        width: {
+            exact: 4096,
+        },
+        height: {
+            exact: 2160,
+        }
+    }
+};
+
 class Camera extends React.Component {
+    constructor(props) {
+        super(props);
+        this.video = null;
+        this.audio = null;
+        this.stream = null;
+    }
+
     openCamera = async (e) => {
+        if (this.video != null) {
+            return;
+        }
+        await this.tryGetUserMedia(constraints);
+        this.handleCamera(this.stream);
+    }
+
+    openAudio = async (e) => {
+        if (this.audio != null) {
+            return;
+        }
+        await this.tryGetUserMedia(constraints);
+        this.handleAudio(this.stream);
+    }
+
+    stopStream = async (e) => {
+        if (this.stream) {
+            this.stream.getTracks().forEach(track => {
+                track.stop();
+            });
+        }
+        this.stream = null;
+        this.video = null;
+        this.audio = null;
+    }
+
+    tryGetUserMedia = async (constraints) => {
         try {
-            const stream = await navigator.mediaDevices.getUserMedia(constraints);
-            console.log('handleSuccess:');
-            this.handleSuccess(stream);
+            this.stream = await navigator.mediaDevices.getUserMedia(constraints);
         } catch (err) {
-            this.handleError(err)
+            this.handleError(err);
         }
     }
 
-    handleSuccess = (stream) => {
-        const video = this.refs['video'];
-        const audio = this.refs['audio'];
+    updateMedia = async (constraints) => {
+        if (this.stream) {
+            this.stream.getTracks().forEach(track => {
+                track.stop();
+            });
+        }
+        await this.tryGetUserMedia(constraints);
+        this.handleCamera(this.stream);
+    }
+
+    handleCamera = (stream) => {
+        this.video = this.refs['video'];
         const videoTracks = stream.getVideoTracks();
-        const audioTracks = stream.getAudioTracks();
-        console.log('The constraints: ', constraints);
+        const track = videoTracks[0];
+        console.log('The constraints: ', track.getConstraints());
         console.log('The camera device in use: ', videoTracks[0].label);
-        console.log('The audio device in use: ', audioTracks[0].label);
         window.stream = stream;
-        video.srcObject = stream;
-        audio.srcObject = stream;
+        this.video.srcObject = stream;
 
         stream.oninactive = () => {
             console.log("Stream paused.");
         }
+    }
+
+    handleAudio = (stream) => {
+        this.audio = this.refs['audio'];
+        const audioTracks = stream.getAudioTracks();
+        console.log('The constraints: ', constraints);
+        console.log('The audio device in use: ', audioTracks[0].label);
+        window.stream = stream;
+        this.audio.srcObject = stream;
+        stream.oninactive = () => {
+            console.log("Stream paused.");
+        }
+    }
+
+    handleResolutionChange = (value) => {
+        console.log(`Selected ${value}`);
+        switch (value) {
+            case 'p240p':
+                this.updateMedia(constraints240p);
+                break;
+            case 'p480p':
+                this.updateMedia(constraints480p);
+                break;
+            case 'p720p':
+                this.updateMedia(constraints720p);
+                break;
+            case 'p1080p':
+                this.updateMedia(constraints1080p);
+                break;
+            case 'p2k':
+                this.updateMedia(constraints2k);
+                break;
+            case 'p4k':
+                this.updateMedia(constraints4k);
+                break;
+        }
+    }
+
+    changeResolution = () => {
+        const track = window.stream.getVideoTracks()[0];
+        console.log('Apply 720p: ', JSON.stringify(constraints720p));
+        track.applyConstraints(constraints720p).then(() => {
+            console.log('Apply 720p successfully...');
+        }).catch(err => {
+            console.log('Apply 720p failed: ', err.name);
+        })
     }
 
     handleError = (error) => {
@@ -42,6 +195,7 @@ class Camera extends React.Component {
             message.error('Please allow using camera');
         }
         message.error(`getUserMedia error: ${error.name}`, error);
+        console.log(`getUserMedia error: ${error.name}`, error);
     }
 
     render() {
@@ -49,9 +203,25 @@ class Camera extends React.Component {
             <div className="container">
                 <h1><span>Use camera</span></h1>
                 <video className="video" ref="video" autoPlay playsInline></video>
+                <div className="container">
+                    <Select defaultValue="p720p" style={{ width: '100px', marginLeft: '20px' }}
+                        onChange={this.handleResolutionChange}>
+                        <Option value="p240p">240p</Option>
+                        <Option value="p480p">480p</Option>
+                        <Option value="p720p">720p</Option>
+                        <Option value="p1080p">1080p</Option>
+                        <Option value="p2k">2k</Option>
+                        <Option value="p4k">4k</Option>
+                    </Select>
+                    <Button onClick={this.changeResolution} style={{ marginLeft: '20px' }}>Dynamic resolution</Button>
+                </div>
                 <audio ref="audio" controls autoPlay></audio>
                 <p className="warning">Warning: please wear headphones to avoid echo.</p>
-                <Button onClick={this.openCamera}>Start camera and microphone.</Button>
+                <div className="container">
+                    <Button onClick={this.openCamera}>Open camera</Button>
+                    <Button onClick={this.openAudio}>Open audio</Button>
+                    <Button onClick={this.stopStream}>Stop</Button>
+                </div>
             </div>
         );
     }
